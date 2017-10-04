febc9381db90eb5a4a4adfc035df4f54f00dc6cd
aa-crypto-ai <aa.crypto-ai@samplemail.com>
2017-10-04 19:31:27 +0800

    Fix player edit profile form - disable email and allow username change

--- player/forms.py
+++ player/forms.py
@@ -2,25 +2,25 @@ from player.models import Player
 from django import forms
 
 class UpdateProfileForm(forms.ModelForm):
-    username = forms.SlugField(required=True, disabled=True)
-    email = forms.EmailField(required=True, max_length=50)
+    username = forms.SlugField(required=True, max_length=50)
+    email = forms.EmailField(required=True, disabled=True)
     nickname = forms.CharField(required=True, max_length=20)
 
     class Meta:
         model = Player
-        fields = ('username', 'email', 'nickname')
+        fields = ('email', 'username', 'nickname')
 
     def clean_email(self):
         username = self.cleaned_data.get('username')
         email = self.cleaned_data.get('email')
 
-        if email and Player.objects.filter(email=email).exclude(username=username).count():
+        if username and Player.objects.filter(username=username).exclude(email=email).count():
             raise forms.ValidationError('This email address is already in use. Please supply a different email address.')
         return email
 
     def save(self, commit=True):
         player = super(UpdateProfileForm, self).save(commit=False)
-        player.email = self.cleaned_data['email']
+        player.username = self.cleaned_data['username']
 
         if commit:
             player.save()
